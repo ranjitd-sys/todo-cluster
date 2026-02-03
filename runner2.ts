81d1ccd54ccc816134508e1a89fd665809ff96ab
ranjit <ranjitd-sys>
2026-02-03 12:10:39 +0530

    new approach

--- runner2.ts
+++ runner2.ts
@@ -1,26 +1,26 @@
-const ShardingLive = Sharding.layer.pipe(
-  Layer.provide(Manager.layer),
-  Layer.provide(Pods.layerMemory),
-  Layer.provide(Storage.layerMemory),
-  Layer.provide(BunClusterSocket.layer()) // <--- Uses Bun's socket implementation
-);
+// const ShardingLive = Sharding.layer.pipe(
+//   Layer.provide(Manager.layer),
+//   Layer.provide(Pods.layerMemory),
+//   Layer.provide(Storage.layerMemory),
+//   Layer.provide(BunClusterSocket.layer()) // <--- Uses Bun's socket implementation
+// );
 
-// --- 4. The Program ---
-const program = Effect.gen(function* () {
-  const client = yield* Counter.client;
-  const entityId = "global-counter";
+// // --- 4. The Program ---
+// const program = Effect.gen(function* () {
+//   const client = yield* Counter.client;
+//   const entityId = "global-counter";
 
-  const n1 = yield* client.increment(entityId);
-  yield* Console.log(`[Bun Cluster] Count is: ${n1}`);
+//   const n1 = yield* client.increment(entityId);
+//   yield* Console.log(`[Bun Cluster] Count is: ${n1}`);
 
-  const n2 = yield* client.increment(entityId);
-  yield* Console.log(`[Bun Cluster] Count is: ${n2}`);
-});
+//   const n2 = yield* client.increment(entityId);
+//   yield* Console.log(`[Bun Cluster] Count is: ${n2}`);
+// });
 
-// --- 5. Run it ---
-program.pipe(
-  Effect.provide(Counter.register(makeCounter)),
-  Effect.provide(ShardingLive),
-  Effect.provide(BunContext.layer),
-  Effect.runPromise
-);
+// // --- 5. Run it ---
+// program.pipe(
+//   Effect.provide(Counter.register(makeCounter)),
+//   Effect.provide(ShardingLive),
+//   Effect.provide(BunContext.layer),
+//   Effect.runPromise
+// );
